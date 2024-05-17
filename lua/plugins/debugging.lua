bf553c253b06609f0fe29159971c5c2eb42c02c6
MateiGoidan <goidan.mateiconstantin@gmail.com>
2024-05-18 01:01:57 +0200

    Configured the LSP for Java

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -28,48 +28,4 @@ return {
 			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
 		end,
 	},
-	{
-		"mfussenegger/nvim-jdtls",
-		ft = "java",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function()
-			local config = {
-				cmd = { "~/.local/share/nvim/mason/bin/jdtls" },
-				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-				on_attach = function(client, bufnr)
-					require("jdtls").setup_dap({ hotcodereplace = "auto" })
-					require("jdtls.dap").setup_dap_main_class_configs()
-					require("jdtls").add_commands()
-				end,
-				init_options = {
-					bundles = {
-						-- Bundle Java debug adapter
-            "~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-0.50.0.jar"
-						-- TODO Bundle java-test
-					},
-				},
-			}
-			require("jdtls").start_or_attach(config)
-
-			-- Setup dap java configuration
-			local dap = require("dap")
-			dap.configurations.java = {
-				{
-					javaExec = "java",
-					request = "launch",
-					type = "java",
-				},
-				{
-					type = "java",
-					request = "attach",
-					name = "Debug (Attach) - Remote",
-					hostName = "127.0.0.1",
-					port = 5005,
-				},
-			}
-		end,
-	},
 }
